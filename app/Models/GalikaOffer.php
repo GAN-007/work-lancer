03fe725d19024fba71cb25d8759c3a0812d0497f
G.A.N. <56300754+GAN-007@users.noreply.github.com>
2026-09-19 14:24:45 +0300

    fix: wire app/Models/GalikaOffer.php to applications

--- app/Models/GalikaOffer.php
+++ app/Models/GalikaOffer.php
@@ -1,4 +1,8 @@
 <?php
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
-class GalikaOffer extends Model{protected $guarded=[];protected $casts=['benefits'=>'array','analysis'=>'array','deadline_at'=>'datetime'];}
+class GalikaOffer extends Model{
+ protected $guarded=[];
+ protected $casts=['benefits'=>'array','analysis'=>'array','deadline_at'=>'datetime'];
+ public function application(){return $this->belongsTo(GalikaApplication::class,'application_id');}
+}
