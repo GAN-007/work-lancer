b434895a819fac5ef0d30f3ec65129d33fb75421
G.A.N. <56300754+GAN-007@users.noreply.github.com>
2026-09-19 14:19:52 +0300

    GALIKA OS: add app/Models/GalikaPrivacyRule.php

---
+++ app/Models/GalikaPrivacyRule.php
@@ -0,0 +1,4 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+class GalikaPrivacyRule extends Model{protected $guarded=[];protected $casts=['allowed'=>'boolean','conditions'=>'array'];}
