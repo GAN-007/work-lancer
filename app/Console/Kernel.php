1f3e707de93bf0d93fe271a5375ac98050bf6032
G.A.N. <56300754+GAN-007@users.noreply.github.com>
2026-09-19 11:09:52 +0300

    GALIKA: schedule continuous opportunity execution

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,32 +1,8 @@
 <?php
-
 namespace App\Console;
-
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-
-class Kernel extends ConsoleKernel
-{
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-        // $schedule->command('inspire')->hourly();
-    }
-
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
-    protected function commands()
-    {
-        $this->load(__DIR__.'/Commands');
-
-        require base_path('routes/console.php');
-    }
+class Kernel extends ConsoleKernel {
+ protected function schedule(Schedule $schedule){$schedule->command('galika:run --limit=50')->everyMinute()->withoutOverlapping(10)->onOneServer()->runInBackground();}
+ protected function commands(){$this->load(__DIR__.'/Commands');require base_path('routes/console.php');}
 }
