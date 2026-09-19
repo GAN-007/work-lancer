94387ba9eda4298abeb75fc3ed13611b337bc9ed
G.A.N. <56300754+GAN-007@users.noreply.github.com>
2026-09-19 14:19:40 +0300

    GALIKA OS: add app/Models/GalikaOffer.php

---
+++ app/Models/GalikaOffer.php
@@ -0,0 +1,4 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+class GalikaOffer extends Model{protected $guarded=[];protected $casts=['benefits'=>'array','analysis'=>'array','deadline_at'=>'datetime'];}
