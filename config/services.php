0dd3cc8b45ecd36c9137ed0b9ca3098c2df696f4
G.A.N. <56300754+GAN-007@users.noreply.github.com>
2026-09-19 11:26:26 +0300

    GALIKA: wire integration service configuration

--- config/services.php
+++ config/services.php
@@ -1,34 +1,11 @@
 <?php
-
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Third Party Services
-    |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
-    |
-    */
-
-    'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
-        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
-        'scheme' => 'https',
-    ],
-
-    'postmark' => [
-        'token' => env('POSTMARK_TOKEN'),
-    ],
-
-    'ses' => [
-        'key' => env('AWS_ACCESS_KEY_ID'),
-        'secret' => env('AWS_SECRET_ACCESS_KEY'),
-        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-    ],
-
+ 'mailgun'=>['domain'=>env('MAILGUN_DOMAIN'),'secret'=>env('MAILGUN_SECRET'),'endpoint'=>env('MAILGUN_ENDPOINT','api.mailgun.net'),'scheme'=>'https'],
+ 'postmark'=>['token'=>env('POSTMARK_TOKEN')],
+ 'ses'=>['key'=>env('AWS_ACCESS_KEY_ID'),'secret'=>env('AWS_SECRET_ACCESS_KEY'),'region'=>env('AWS_DEFAULT_REGION','us-east-1')],
+ 'openai'=>['key'=>env('OPENAI_API_KEY')],
+ 'gmail'=>['access_token'=>env('GMAIL_ACCESS_TOKEN'),'from'=>env('GMAIL_FROM')],
+ 'airtable'=>['token'=>env('AIRTABLE_TOKEN'),'base_id'=>env('AIRTABLE_BASE_ID')],
+ 'tinyfish'=>['key'=>env('TINYFISH_API_KEY'),'endpoint'=>env('TINYFISH_ENDPOINT')],
+ 'jobicy'=>['endpoint'=>env('JOBICY_ENDPOINT','https://jobicy.com/api/v2/remote-jobs')],
 ];
